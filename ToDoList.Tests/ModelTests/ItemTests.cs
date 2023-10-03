f83705b0e0dd601dc8bd546dfbb8e12ea756fc2c
Brian Scherner <brianscherner@gmail.com>
2023-10-03 15:21:10 -0700

    Add functionality for finding ID for Item + passing test.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -89,5 +89,16 @@ namespace ToDoList.Tests
       int result = newItem.Id;
       Assert.AreEqual(1, result);
     }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem01 = new Item(description01);
+      Item newItem02 = new Item(description02);
+      Item result = Item.Find(2);
+      Assert.AreEqual(newItem02, result);
+    }
   }
 }
