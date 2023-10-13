f52fd275e64da3815bf12042100c304982bb8015
Brian Scherner <brianscherner@gmail.com>
2023-10-13 10:26:50 -0700

    Add functionality for finding Item by ID in database + passing test.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -19,42 +19,42 @@ namespace ToDoList.Tests
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=3picodu$4991;port=3306;database=to_do_list_test;";
     }
 
-  //   [TestMethod]
-  //   public void ItemConstructor_CreatesInstanceOfItem_Item()
-  //   {
-  //     Item newItem = new Item("test");
-  //     Assert.AreEqual(typeof(Item), newItem.GetType());
-  //   }
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
 
-  //   [TestMethod]
-  //   public void GetDescription_ReturnsDescription_String()
-  //   {
-  //     //Arrange
-  //     string description = "Walk the dog.";
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
 
-  //     //Act
-  //     Item newItem = new Item(description);
-  //     string result = newItem.Description;
+      //Act
+      Item newItem = new Item(description);
+      string result = newItem.Description;
 
-  //     //Assert
-  //     Assert.AreEqual(description, result);
-  //   }
+      //Assert
+      Assert.AreEqual(description, result);
+    }
 
-  //   [TestMethod]
-  //   public void SetDescription_SetDescription_String()
-  //   {
-  //     //Arrange
-  //     string description = "Walk the dog.";
-  //     Item newItem = new Item(description);
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
 
-  //     //Act
-  //     string updatedDescription = "Do the dishes";
-  //     newItem.Description = updatedDescription;
-  //     string result = newItem.Description;
+      //Act
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
 
-  //     //Assert
-  //     Assert.AreEqual(updatedDescription, result);
-  //   }
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
+    }
 
     [TestMethod]
     public void GetAll_ReturnsEmptyListFromDatabase_ItemList()
@@ -88,25 +88,26 @@ namespace ToDoList.Tests
       CollectionAssert.AreEqual(newList, result);
     }
 
-  //   [TestMethod]
-  //   public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
-  //   {
-  //     string description = "Walk the dog";
-  //     Item newItem = new Item(description);
-  //     int result = newItem.Id;
-  //     Assert.AreEqual(1, result);
-  //   }
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      string description = "Walk the dog";
+      Item newItem = new Item(description);
+      newItem.Save();
+      Item foundItem = Item.Find(newItem.Id);
+      Assert.AreEqual(foundItem.Id, newItem.Id);
+    }
 
-  //   [TestMethod]
-  //   public void Find_ReturnsCorrectItem_Item()
-  //   {
-  //     string description01 = "Walk the dog";
-  //     string description02 = "Wash the dishes";
-  //     Item newItem01 = new Item(description01);
-  //     Item newItem02 = new Item(description02);
-  //     Item result = Item.Find(2);
-  //     Assert.AreEqual(newItem02, result);
-  //   }
+    [TestMethod]
+    public void Find_ReturnsCorrectItemFromDatabase_Item()
+    {
+      Item newItem = new Item("Mow the lawn");
+      newItem.Save();
+      Item newItem2 = new Item("Wash dishes");
+      newItem2.Save();
+      Item foundItem = Item.Find(newItem.Id);
+      Assert.AreEqual(newItem, foundItem);
+    }
 
     [TestMethod]
     public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
