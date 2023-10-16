029986fd2b8dac71942d5eaba4efc88e43cf3768
Brian Scherner <brianscherner@gmail.com>
2023-10-16 15:04:47 -0700

    Add functionality for returning Item List + passing test.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -25,10 +25,9 @@ namespace ToDoList.Models
       else
       {
         Item newItem = (Item)otherItem;
-        // bool idEquality = (this.Id == newItem.Id);
+        bool idEquality = (this.Id == newItem.Id);
         bool descriptionEquality = (this.Description == newItem.Description);
-        // return (idEquality && descriptionEquality);
-        return descriptionEquality;
+        return (idEquality && descriptionEquality);
       }
     }
 
