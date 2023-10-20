de57a846cfd85f21d9044192f0cc08a73161d747
Brian Scherner <brianscherner@gmail.com>
2023-10-20 11:51:03 -0700

    Create new views for Categories, create controller and config.

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel;
 using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
+    public DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
