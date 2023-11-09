98d6f909a7de2404bc2132172d2c44209082d4e0
Brian Scherner <brianscherner@gmail.com>
2023-11-09 14:56:23 -0800

    Add splash page to project.

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly ToDoListContext _db;
+      public HomeController(ToDoListContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        Category[] cats = _db.Categories.ToArray();
+        Item[] items = _db.Items.ToArray();
+        Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+        model.Add("categories", cats);
+        model.Add("items", items);
+        return View(model);
       }
 
     }
